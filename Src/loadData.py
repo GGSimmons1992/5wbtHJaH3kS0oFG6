a9a07a2764e663d712ba40dad3149f33760991d8
Gary Simmons <gary.simmons@colorado.edu>
2024-10-27 19:19:12 -0700

    update loadData.py and train AR, arima, sarima, and exponentialSmoothing models

--- Src/loadData.py
+++ Src/loadData.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import numpy as np
 import pandas as pd
 import yfinance as yf
@@ -5,13 +11,21 @@ import yfinance as yf
 import sys
 sys.path.insert(0, "../Src/")
 
-%autosave 5
+get_ipython().run_line_magic('autosave', '5')
+
+
+# In[2]:
 
 
 def loadData(ticker):
     train = yf.download(ticker, start="2020-01-01", end="2024-03-01")[['Close']]
     test = yf.download(ticker, start="2024-03-01", end="2024-10-13")[['Close']]
+    train.reset_index(drop=True, inplace=True)
+    test.reset_index(drop=True, inplace=True)
     return train,test
+
+
+# In[3]:
 
 
 def main():
@@ -20,4 +34,8 @@ def main():
     display(test)
 
 
-main()
+# In[4]:
+
+if __name__ == '__main__':
+    main()
+
