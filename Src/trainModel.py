8c24d9f7c3c7c4b4b0972b99c2ea6e24cc7edf2d
Gary Simmons <gary.simmons@colorado.edu>
2024-12-16 00:31:57 -0800

    fix simpler models with incremental predictions and attempt the same for prophet

---
+++ Src/trainModel.py
@@ -0,0 +1,341 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import warnings
+import tensorflow as tf
+warnings.filterwarnings('ignore')
+tf.get_logger().setLevel('ERROR')
+import numpy as np
+import pandas as pd
+import yfinance as yf
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.graphics.tsaplots import plot_acf,plot_pacf
+from statsmodels.tsa.stattools import adfuller
+import matplotlib.pyplot as plt
+import pmdarima as pm
+from statsmodels.tsa.api import AutoReg
+import pickle
+from statsmodels.tsa.holtwinters import SimpleExpSmoothing, Holt, ExponentialSmoothing
+from sklearn.metrics import mean_squared_error
+from prophet import Prophet
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+import json
+import os
+from os.path import exists
+
+import sys
+sys.path.insert(0, "../Src/")
+import loadData
+
+get_ipython().run_line_magic('autosave', '5')
+
+
+# In[2]:
+
+
+def stabilize(data):
+    p = 100
+    while p > 0.05:
+        adfTest = adfuller(data)
+        p = adfTest[1]
+        print(f'p: {adfTest[1]}')
+        if p > 0.05:
+            data = data.diff().dropna()
+    return data
+
+
+# In[3]:
+
+
+def trainArima(data,params):
+    arima = ARIMA(data, **params)
+    arimaFit = arima.fit()
+    return arimaFit
+
+
+# In[4]:
+
+
+def trainBestArima(data):
+    print('Training ARIMA')
+    data = stabilize(data)
+    possibleP = np.arange(5)
+    possibleD = np.arange(5)
+    possibleQ = np.arange(5)
+
+    train, dev = trainDevSplit(data)
+
+    bestArima = None
+    bestmse = np.inf
+    bestP, bestQ = 0, 0
+
+    for _ in range(20):
+        p = np.random.choice(possibleP)
+        d = np.random.choice(possibleD)
+        q = np.random.choice(possibleQ)
+        params = {'order': (p, d, q)}
+        arima = ARIMA(train, order=(p, d, q))
+        arimaFit = trainArima(train,params)
+        forecast = arimaFit.forecast(len(dev))
+        mse = mean_squared_error(dev, forecast)
+        if mse < bestmse:
+            bestmse = mse
+            bestP, bestD, bestQ = p, d, q
+
+    bestArimaParams = {
+        'order': (int(bestP), int(bestD), int(bestQ))
+    }
+    with open('../Models/arima_params.json', 'w') as f:
+        json.dump(bestArimaParams, f)
+    
+    arimaFit = trainArima(train,bestArimaParams)
+    print(arimaFit.summary())
+    residuals = arimaFit.resid[1:]
+
+    fig,ax = plt.subplots(1,2)
+    residuals.plot(title = 'Residuals', ax = ax[0])
+    residuals.plot(title = 'Density', kind = 'kde', ax = ax[1])
+
+    saveModel(arimaFit, 'arima')
+    return arimaFit
+
+
+# In[5]:
+
+
+def saveModel(model,name):
+    filename = f'../Models/{name}.pkl'
+    if name in ['autoRegression','sarima','ExponentialSmoothing', 'prophet']:
+        with open(filename, 'wb') as pkl:
+            pickle.dump(model, pkl)
+    else:
+        model.save(filename)
+
+
+# In[6]:
+
+
+def trainSarima(train,saveSarima = False):
+    print('Training Sarima')
+    sarima = pm.auto_arima(train,stepwise=True,seasonal=True)
+    print(sarima.summary())
+    if (saveSarima):
+        saveModel(sarima,'sarima')
+    return sarima
+
+
+# In[7]:
+
+
+def trainAutoRegression(data,params):
+    return AutoReg(data,**params).fit()
+
+
+# In[8]:
+
+
+def trainBestAutoRegression(data):
+    print('Training AutoRegression')
+    bestLag = 0
+    bestCorr = 0
+    for lag in range(1,11):
+        corr = data.corr(data.shift(lag))
+        if corr > bestCorr:
+            bestLag = lag
+            bestCorr = corr
+
+    bestARParams = {
+        'lags':bestLag
+    }
+    with open('../Models/ar_params.json', 'w') as f:
+        json.dump(bestARParams, f)
+    
+    print(f'AR order = {bestLag}')
+    ar_model = trainAutoRegression(data,bestARParams)
+    saveModel(ar_model,'autoRegression')
+    return ar_model
+    
+
+
+# In[9]:
+
+
+def trainExponentialSmoothing(data,params):
+    modelName = params['modelName']
+    params = {
+        'smoothing_level': params['smoothing_level']
+    }
+    constructors = {
+        "SimpleExpSmoothing": SimpleExpSmoothing,
+        "Holt": Holt,
+        "ExponentialSmoothing": ExponentialSmoothing
+    }
+    model = constructors[modelName](data)
+    return model.fit(**params)
+
+
+# In[10]:
+
+
+def trainBestExponentialSmoothing(data):
+    print('Training ExponentialSmoothing')
+    arrayData = np.asarray(data)
+    trainDevCutOff = int(len(arrayData) * 0.8)
+    train = arrayData[:trainDevCutOff]
+    dev = arrayData[trainDevCutOff:]
+
+    possibleModelNames = ["SimpleExpSmoothing", "Holt", "ExponentialSmoothing"]
+    bestmse = np.inf
+
+    bestModelParams = {
+        'modelName': '',
+        'smoothing_level': ''
+    }
+    for _ in range(20):
+        smoothingLevel = np.random.uniform(0.1, 0.9)
+        modelIndex = np.random.choice(len(possibleModelNames))
+        params = {
+            'modelName': possibleModelNames[modelIndex],
+            'smoothing_level': smoothingLevel
+        }
+        
+        model_fit = trainExponentialSmoothing(train,params)
+        forecast = model_fit.forecast(len(dev))
+        mse = mean_squared_error(dev, forecast)
+        if mse < bestmse:
+            bestmse = mse
+            bestModelParams = params
+            
+    print(f'model chosen {bestModelParams["modelName"]}')
+    print(f'smoothing parameter {bestModelParams["smoothing_level"]}')
+    
+    with open('../Models/exponentialSmoothing_params.json', 'w') as f:
+        json.dump(bestModelParams, f)
+    
+    final_model_fit = trainExponentialSmoothing(train,bestModelParams)
+    
+    saveModel(final_model_fit, 'ExponentialSmoothing')
+    return final_model_fit
+    
+
+
+# In[11]:
+
+
+def trainProphet(data,saveModelFile=False):
+    if (saveModelFile):
+        print('Training Prophet')
+
+    model = Prophet(seasonality_mode='multiplicative')
+    model.fit(data)
+    if (saveModelFile):
+        saveModel(model, 'prophet')
+    return model
+        
+
+
+# In[12]:
+
+
+def trainLSTM(data):
+    train, dev = trainDevSplit(data)
+    XData, yData = loadData.processDataForLSTM(data)
+    XTrain, yTrain = loadData.processDataForLSTM(train)
+    XDev, yDev = loadData.processDataForLSTM(dev)
+    
+    bestTrainScore = np.inf
+    bestDevScore = np.inf
+    bestLSTMUnits1 = 0
+    bestLSTMUnits2 = 0
+    bestLSTMUnits3 = 0
+    bestEpochs = 0
+    for trial in range(30):    
+        lstmUnits1 = np.random.choice(range(1,128))
+        lstmUnits2 = np.random.choice(range(1,128))
+        lstmUnits3 = np.random.choice(range(1,128))
+        epochs = np.random.choice(range(1,100))
+
+        model = compileLSTM(XTrain,yTrain,lstmUnits1,lstmUnits2,lstmUnits3,epochs)
+        
+        devScore = model.evaluate(XDev,yDev)[1]
+        if devScore < bestDevScore:
+            bestTrainScore = model.evaluate(XTrain,yTrain)[1]
+            bestDevScore = devScore
+            bestLSTMUnits1 = lstmUnits1
+            bestLSTMUnits2 = lstmUnits2
+            bestLSTMUnits3 = lstmUnits3
+            bestEpochs = epochs
+    compileLSTM(XData,yData,bestLSTMUnits1,bestLSTMUnits2,lstmUnits3,bestEpochs)
+    
+    model_path = f'../Models/LSTM_.h5'
+    model.save(model_path)
+    bestModelParams = {
+        'bestTrainScore': int(bestTrainScore),
+        'bestDevScore': int(bestDevScore),
+        'bestLSTMUnits1': int(bestLSTMUnits1),
+        'bestLSTMUnits2': int(bestLSTMUnits2),
+        'bestLSTMUnits3': int(bestLSTMUnits3),
+        'bestEpochs': int(bestEpochs)
+    }
+    with open('../Models/lstm_params.json', 'w') as f:
+        json.dump(bestModelParams, f)
+    
+    return model
+        
+
+
+# In[13]:
+
+
+def trainDevSplit(data):
+    totalRows = data.shape[0]
+    trainDevCutoff = int(0.8 * totalRows)
+    train = data.iloc[:trainDevCutoff]
+    dev = data.iloc[trainDevCutoff:]
+    return train,dev
+
+
+# In[14]:
+
+
+def compileLSTM(X,y,lstmUnits1,lstmUnits2,lstmUnits3,epochs):
+    model = Sequential()
+    model.add(LSTM(units=lstmUnits1, return_sequences=True, input_shape=(X.shape[1], 1)))
+    model.add(LSTM(units=lstmUnits2, return_sequences=True))
+    model.add(LSTM(units=lstmUnits3))
+    model.add(Dense(units=1))
+    model.compile(optimizer='adam', loss='mean_squared_error',metrics=['mean_squared_error'])
+    model.fit(X, y, epochs=epochs, batch_size=32,verbose=0)
+    return model
+
+
+# In[15]:
+
+
+def main():
+    train, test = loadData.splitData(loadData.loadData('MSFT'))
+    if exists('../Models/autoRegression.pkl') == False:
+        trainBestAutoRegression(train['y'])
+    if exists('../Models/arima.pkl') == False:
+        trainBestArima(train['y'])
+    if exists('../Models/sarima.pkl') == False:
+        trainSarima(train['y'],True)
+    if exists('../Models/ExponentialSmoothing.pkl') == False:
+        trainBestExponentialSmoothing(train['y'])
+    if exists('../Models/prophet.pkl') == False:
+        trainProphet(train,True)
+    
+    if exists('../Models/LSTM_.h5') == False:
+        trainLSTM(train['y'])
+
+
+# In[16]:
+
+
+if __name__ == '__main__':
+    main()
+
--- Src/visualization.py
+++ Src/visualization.py
@@ -1,9 +1,14 @@
+import json
+import loadData
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_absolute_error
 from statsmodels.tsa.seasonal import seasonal_decompose
 import pickle
 import pandas as pd
+import trainModel as tm
+import pmdarima as pm
+from IPython.display import display
 
 def reshapeTo1DArray(data):
     return np.array(data).reshape(-1,)
@@ -48,9 +53,14 @@ def extractSeasonality(data):
     result = seasonal_decompose(data, model='stl', period=20)
     return result.seasonal
 
-def loadModel(modelName):
-    with open(f'../Models/{modelName}.pkl', 'rb') as file:
-        model = pickle.load(file)
+def loadModel(data,modelName,params):
+    model = None
+    if modelName == 'Autoregression':
+        model = tm.trainAutoRegression(data,params)
+    elif modelName == 'ARIMA':
+        model = tm.trainArima(data,params)
+    elif modelName == 'Exponential Smoothing':
+        model = tm.trainExponentialSmoothing(data,params)
     return model
 
 def compare(date,test,predict,modelName):
@@ -69,13 +79,45 @@ def compare(date,test,predict,modelName):
     print(plotComparison(date,seasonalTest,seasonalPredict,'RawSeasonality',f'{modelName} Raw Seasonality'))
     print(plotComparison(date,seasonalScaledTest,seasonalScaledPredict,'ScaledSeasonality',f'{modelName} Scaled Seasonality'))
 
-def compareSimplePickleModel(fileName,date,validation,modelName):
-    model = loadModel(fileName)
-    if fileName == "sarima":
-        predict = model.predict(n_periods=len(validation))
-    else:
-        predict = model.forecast(len(validation))
-    compare(date,validation,predict,modelName)
+def makePrediction(data,modelName,params,periods):
+    if modelName == "SARIMA":
+        model = pm.auto_arima(data,stepwise=True,seasonal=True)
+        predict = model.predict(n_periods=periods)
+    else:  
+        model = loadModel(data,modelName,params)
+        predict = model.forecast(periods)
+    return list(np.array(predict).reshape(-1,))
+        
 
-    
+def compareSimplePickleModel(data,modelName,paramsFile = ''):
+    train,validation = loadData.splitData(data)
+    fullPredict = []
+    continueTraining = True
+    params = {}
+    if paramsFile != '':
+        with open(f'../Models/{paramsFile}.json') as d:
+            params = json.load(d)
+    periods = 10
+    while (continueTraining):
+        predict = makePrediction(train['y'],modelName,params,periods)
+        fullPredict += predict
+        train = data.iloc[:(train.shape[0]+periods)]
+        if ((train.shape[0] + periods ) >= data.shape[0]):
+            periods = data.shape[0] - train.shape[0]
+            predict = makePrediction(train['y'],modelName,params,periods)
+            fullPredict += predict
+            continueTraining = False
+            
+    print('len(fullPredict) ',len(fullPredict))
+    print("len(validation['y']) ",len(validation['y']))
+    compare(validation['ds'],validation['y'],fullPredict,modelName)
+
+def makeProphetPrediction(data,periods):
+    model = tm.trainProphet(data)
+    future = model.make_future_dataframe(periods=periods,include_history=False)
+    #capValue = test['y'].max() * 1.1
+    #future['cap'] = capValue
+    #future['floor'] = 0
+    print('future.shape ',future.shape)
+    return model.predict(future)
 
